d179e18187eba59369318bcd49d4fc27941874d5
Islam <isxmad@gmail.com>
2023-03-08 20:27:32 +0300

    localization

--- hoc/checkLoc.tsx
+++ hoc/checkLoc.tsx
@@ -1,20 +1,26 @@
 import { useEffect } from "react";
- 
 import ApiService from "@/service/apiService";
 
 
 const service = new ApiService();
 
+const ruCnt = ['AZ', 'AM', 'BY', 'KZ', 'KG', 'MD', 'RU', 'TM', 'TJ', 'UZ', 'UA']
+
 
 const CheckLoc = ({children}: {children: React.ReactNode}) => {
     
 
-    // useEffect(() => {
-    //     service.getIp().then(res => {
-    //         console.log(res)
+    useEffect(() => {
+        service.getIp().then(res => {
+            // console.log(res)
+            if(ruCnt.find(i => i === res?.country_code)) {
+                return
+            } else {
+                
+            }
 
-    //     })
-    // }, [])
+        })
+    }, [])
 
     return (    
         <>
